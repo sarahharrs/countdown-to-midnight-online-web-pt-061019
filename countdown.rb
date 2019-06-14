775642e675f3dcd59787542e38abab630a3553f3
Sarah Harrs <sarahharrs@gmail.com>
2019-06-14 22:07:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@ def countdown (seconds_to_midnight)
   number = 5
   while seconds_to_midnight > 0
   puts "#{seconds_to_midnight} SECOND(S)!"
-  number -= 1
+  seconds_to_midnight-= 1
 end 
   puts "HAPPY NEW YEAR!"
 end
